d7762c996672b36578363ea74728ca266fdc7b36
linah.muhonja <linahmuhonjaimbari@gmail.com>
2023-01-10 23:47:11 +0300

    add database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_08_091107) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_10_203740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.text "first_name"
+    t.text "email_address"
+    t.integer "phone_number"
+    t.time "check_in_time"
+    t.integer "person_capacity"
+    t.text "additional_info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
