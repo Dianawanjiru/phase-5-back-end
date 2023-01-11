c656c10f92f85103c972cf308239dde8779022f5
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2023-01-11 06:27:22 +0300

    include has_many association macro

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,3 +1,5 @@
 class CarSerializer < ActiveModel::Serializer
   attributes :id, :price, :make, :model, :fuel_type, :transmission, :mileage, :engine_size, :year, :image
+  has_many :users
+  has_many :reservations
 end
