3a5a58dcb0d69162f5213396b956e8ac25553132
linah.muhonja <linahmuhonjaimbari@gmail.com>
2023-01-11 16:09:22 +0300

    add appointment serializer

---
+++ app/serializers/appointment_serializer.rb
@@ -0,0 +1,3 @@
+class AppointmentSerializer < ActiveModel::Serializer
+  attributes :first_name, last_name, :email_address, :phone_number, :check_in_date, :check_in_time, :person_capacity, :additional_info
+end
