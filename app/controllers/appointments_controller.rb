d853d69580dc23de9e21e2eeeb59808af4d69346
linah.muhonja <linahmuhonjaimbari@gmail.com>
2023-01-11 18:20:03 +0300

    add appointment contoller

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,44 @@
+class AppointmentsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with:  :respond_record_not_found
+     
+    def index 
+        appointments = Appointment.all
+        render json: appointments
+    end
+
+    def show 
+        appointment = Appointment.find(params[:id])        
+        render json: appointment, status: 200
+    end
+
+    def create
+        appointment = @current_user.appointment.create!(appointment_params)
+        render json: appointment, status: :created
+    end
+
+    def update 
+        appointment = Appointment.find_by(id: params[:id])
+        if appointment
+            appointment.update(appointment_params)
+            render json: appointment
+        else
+            render json: { error: "Bird not found" }, status: :not_found
+        end
+    end
+
+    def destroy
+        session.delete :appointment_id
+        head :no_content
+    end
+
+
+
+    private
+    def appointment_params
+        params.permit(:first_name, :email_address, :phone_number, :check_in_time, :person_capacity, :additional_info, :last_name, :check_in_date, :user_id)
+    end
+
+    def respond_record_not_found
+        render json: { error: "appointment not found" }, status: 404
+    end
+end
