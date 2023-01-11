ad4944018a8d549ceeb9abad2ce12fd0223ea4ba
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2023-01-11 19:02:26 +0300

    include association macros to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -9,4 +9,7 @@ class Reservation < ApplicationRecord
     validates :check_out_date, presence:true
     validates :check_in_time, presence: true
     validates :check_out_time, presence:true
+
+    belongs_to :user
+    belongs_to :car
 end
