febec7ca79f948c7d5802a07c756cdbcf37618f5
linah.muhonja <linahmuhonjaimbari@gmail.com>
2023-01-11 16:13:32 +0300

    add appointment seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+class Seed 
+    def self.begin
+        seed = Seed.new
+        seed.generate_users
+        seed.generate_appointments
+        
+    end
+    def generate_users
+        3.times do |i| 
+            user = User.create!(
+                username:Faker::Name.name,
+                password_digest:Faker::Internet.password,
+                image_url: Faker::Internet.url,
+                bio:Faker::Lorem.paragraph(sentence_count: 4)
+            )
+        end
+    end
+
+    def generate_appointments
+        5.times do |i|
+            appointment = Appointment.create!(
+                first_name:Faker::Name.name,
+                last_name:Faker::Name.name,
+                email_address:Faker::Internet.email,
+                phone_number:Faker::PhoneNumber.cell_phone,
+                check_in_date:Faker::Date.forward(days: 23),
+                check_in_time:Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default),
+                person_capacity:Faker::Number.between(from: 1, to: 7),
+                additional_info:Faker::Lorem.paragraph(sentence_count: 4),
+                user_id:Faker::Number.between(from: 1, to: 5) 
+            )
+        end
+    end
+
+    
+end
+
+Seed.begin
+
+
