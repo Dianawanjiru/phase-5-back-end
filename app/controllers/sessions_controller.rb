93a56ce9c876af507ff4570224b91c8a78d22278
linah.muhonja <linahmuhonjaimbari@gmail.com>
2023-01-15 16:51:10 +0300

    add routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,19 @@
 class SessionsController < ApplicationController
-  include ActionController::Cookies
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  skip_before_action :authorize, only: :create
 
-  def authorize
-    render json: { errors: ["Not authorized"] }, status: :unauthorized unless session[:user_id]
+  def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+    end
   end
 
-  def render_not_found_response
-    render json: { error: "Car not found" }, status: :not_found
+  def destroy
+    session.delete :user_id
+    head :no_content
   end
 
-  def render_unprocessable_entity(invalid)
-    return render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
-  end
 end
