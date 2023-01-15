7325233f9254ef6e245f42ab90e61083c2c19cc8
linah.muhonja <linahmuhonjaimbari@gmail.com>
2023-01-15 16:49:47 +0300

    add skip_before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,20 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+  
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  before_action :authorize
+
+  private
 
   def authorize
-    render json: { errors: ["Not authorized"] }, status: :unauthorized unless session[:user_id]
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
   end
 
-  def render_not_found_response
-    render json: { error: "Car not found" }, status: :not_found
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 
-  def render_unprocessable_entity(invalid)
-    return render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
-  end
 end
