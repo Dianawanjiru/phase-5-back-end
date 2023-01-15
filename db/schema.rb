e7df29e1a8b5561dfd82dc13a757f929a632353e
linah.muhonja <linahmuhonjaimbari@gmail.com>
2023-01-15 17:06:21 +0300

    remove bio and image_url attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_130255) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_140311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,8 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_130255) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.string "image_url"
-    t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
