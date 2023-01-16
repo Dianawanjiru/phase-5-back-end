c9a7ea519a6d9e080edb4d0cc1287e280d070688
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2023-01-16 09:43:04 +0300

    refactor code

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -17,7 +17,7 @@ class ReservationsController < ApplicationController
     end
 
     def update 
-        reservation = Reservation.find_by(id: params[:id])
+        reservation = @current_user.reservation.find_reservation
         if reservation
             reservation.update(reservation_params)
             render json: reservation
@@ -27,11 +27,20 @@ class ReservationsController < ApplicationController
     end
 
     def destroy
-        session.delete :reservation_id
-        head :no_content
+        reservation = @current_user.find_reservation
+        if reservation
+            reservation.destroy
+            head :no_content
+        else
+            render json: { error: "reservation not found" }, status: :not_found
+        end
     end
 
     private
+    def find_reservation
+        Reservation.find_by(id: params[:id])
+    end
+
     def reservation_params
         params.permit(:first_name, :last_name, :email_address, :phone_number, :from_adress, :to_adress, :check_in_date, :check_out_date, :check_in_time, :check_out_time, :additional_information, :user_id, :car_id)
     end
